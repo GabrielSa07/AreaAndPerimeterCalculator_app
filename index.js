55c7451b65d5164b993b6bca29e7009d14074efa
Gabriel Bento de Sá <103396526+GabrielSa07@users.noreply.github.com>
2023-09-19 23:15:21 -0300

    rename and remove buttonCalculator

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
+import { selectCircle, selectDiamond, selectRectangle, selectSquare, selectTrapeze, selectTriangle } from "./src/buttonDOM.js"
+
+
+
 const button = document.querySelector('#select-button')
 const select = document.querySelector('#options')
 
-
 button.addEventListener('click', (ev) => {
+    ev.preventDefault()
+
+    switch(select.value) {
+        case 'triangle':
+            selectTriangle()
+        break;
+        case 'rectangle':
+            selectRectangle()
+        break;
+        case 'square':
+            selectSquare()
+        break;
+        case 'circle':
+            selectCircle()
+        break;
+        case 'trapeze':
+            selectTrapeze()
+        break;
+        case 'diamond':
+            selectDiamond()
+        break;
+    }
+})
+
+
+const buttonCalc = document.querySelector('#button-calculator')
+
+buttonCalc.addEventListener('click', (ev) => {
     ev.preventDefault()
 
     switch(select.value) {
